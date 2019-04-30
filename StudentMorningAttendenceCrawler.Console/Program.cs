74b7ac1a44a38adf445be93b49576e2fd2cc21fb
Laggage <1634205628@qq.com>
2019-04-30 17:55:06 +0800

    Initially complete

---
+++ StudentMorningAttendenceCrawler.Console/Program.cs
@@ -0,0 +1,263 @@
+﻿using StudentMorningAttendenceCrawler.Core;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Net;
+using System.Linq;
+
+namespace StudentMorningAttendenceCrawler.Console
+{
+    using Console = System.Console;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dictionary<string, string> para = new Dictionary<string, string>
+            //{
+            //    {"dispalyName","" },
+            //    {"displayPasswd","" },
+            //    {"submit","%B5%C7++%C2%BC" },
+            //    {"userName","Z05117203" },
+            //    {"userPasswd","Z05117203" }
+            //};
+            //StringBuilder sb = new StringBuilder();
+            //foreach(var i in para)
+            //{
+            //    sb.Append(string.Format("{0}={1}&", i.Key.Escape(), i.Value.Escape()));
+            //}
+
+
+            //CookieContainer cookies = new CookieContainer();
+            //HttpWebRequest loginReq = WebRequest.Create(loginPageUrl) as HttpWebRequest;
+            //loginReq.CookieContainer = new CookieContainer();
+            //loginReq.KeepAlive = true;
+            ////loginReq.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3";
+            //loginReq.Headers.Add(HttpRequestHeader.AcceptLanguage, "zh-CN,zh;q=0.9,en;q=0.8");
+            //loginReq.Headers.Add(HttpRequestHeader.AcceptEncoding, "gzip, deflate");
+            //loginReq.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36";
+            //loginReq.Method = "Get";
+
+            //HttpWebResponse loginRes = loginReq.GetResponse() as HttpWebResponse;
+            //cookies = loginReq.CookieContainer;
+            //string cookieStr = cookies.GetCookieHeader(loginReq.RequestUri);
+            //System.Console.WriteLine(cookieStr);
+            ////loginRes.Close();
+            ////loginRes.Dispose();
+            //loginReq.AllowAutoRedirect = true;
+
+            //HttpWebRequest req = WebRequest.Create(queryUrl) as HttpWebRequest;
+            //req.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3";
+            //req.AllowAutoRedirect = false;
+            //req.ContentType = "application / x - www - form - urlencoded";
+            ////req.Host = "10.28.102.51";
+            //req.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36";
+            //req.Method = "POST";
+            //req.KeepAlive = true;
+            ////req.CookieContainer = new CookieContainer();
+            ////req.CookieContainer.Add(req.RequestUri,cookies.GetCookies(loginReq.RequestUri));
+            //req.Headers.Add("Cookie", cookieStr);
+            //req.Headers.Add(HttpRequestHeader.AcceptEncoding, "gzip, deflate");
+            //req.Headers.Add("Origin", "http://10.28.102.51");
+            ////req.Headers.Add("Upgrade-Insecure-Requests", "1");
+            //req.Headers.Add(HttpRequestHeader.AcceptLanguage, "zh-CN,zh;q=0.9,en;q=0.8");
+            //byte[] data = Encoding.Default .GetBytes(sb.ToString().TrimEnd('&'));
+            //req.ContentLength = data.Length;
+            ////req.ProtocolVersion = HttpVersion.Version11;
+
+            ////string s = req.CookieContainer.GetCookieHeader(req.RequestUri);
+            //Stream reqStream = req.GetRequestStream();
+
+
+            //reqStream.Write(data, 0, data.Length);
+            //reqStream.Flush();
+            //reqStream.Close();
+
+            //System.Console.WriteLine(Encoding.Default.GetString(data));
+
+            ////System.Console.WriteLine(reader.ReadToEnd());
+
+            //HttpWebResponse res = req.GetResponse() as HttpWebResponse;
+            //Stream resStream = res.GetResponseStream();
+            //StreamReader streamReader = new StreamReader(resStream, Encoding.Default);
+            //string text = streamReader.ReadToEnd();
+            //System.Console.WriteLine(text);
+            //System.Console.WriteLine("Status : " + res.StatusCode);
+            //System.Console.ReadKey();
+            //res.Close();
+            //res.Dispose();
+
+            //RestClient client = new RestClient
+            //{
+            //    BaseUrl = new Uri(loginPageUrl),
+            //    CookieContainer = new CookieContainer(),
+            //    Encoding = Encoding.GetEncoding("gb2312")
+            //};
+
+            //RestRequest loginReq = new RestRequest(Method.GET);
+            //client.Get(loginReq);
+
+            //RestRequest request = new RestRequest(Method.POST);
+            //request.AddHeader("Accept-Language", "zh-CN,zh;q=0.9,en;q=0.8");
+            //request.AddHeader("Accept-Encoding", "gzip, deflate");
+            //request.AddHeader("cache-control", "no-cache");
+            //request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+            ////request.AddHeader("Cookie", "EC01F06113A100C26442EE128F42250C");
+            //request.AddHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36");
+            //request.AddParameter("dispalyName", "");
+            //request.AddParameter("displayPasswd", "");
+            //request.AddParameter("submit", "%B5%C7++%C2%BC");
+            //request.AddParameter("userName", "Z05117203");
+            //request.AddParameter("passwd", "Z05117203");
+            //System.Console.WriteLine(client.CookieContainer.GetCookieHeader(client.BaseUrl));
+            //IRestResponse response = client.Execute(request);
+
+            //response.ContentEncoding = "gb2312";
+            //string html = Encoding.Default.GetString(response.RawBytes);
+
+            //System.Console.WriteLine(html);
+            Program p = new Program();
+            p.Run();
+        }
+
+        internal enum QueryMode {
+            QueryByClass,
+            QUeryByPerson
+        }
+
+        internal void GetQueryMode(out QueryMode mode)
+        {
+            mode = QueryMode.QUeryByPerson;
+            int res = 0;
+            Console.WriteLine("1.班级查询.");
+            Console.WriteLine("2.个人查询");
+            while (!int.TryParse(Console.ReadLine(), out res) && 
+                !(res == 1 || res == 2)) ;
+            mode = res == 1 ? QueryMode.QueryByClass : QueryMode.QUeryByPerson;
+        }
+
+        internal void Run()
+        {
+            while(true)
+            {
+                Console.Clear();
+                GetQueryMode(out QueryMode mode);
+                switch (mode)
+                {
+                    case QueryMode.QUeryByPerson:
+                        QueryByPerson();
+                        break;
+                    case QueryMode.QueryByClass:
+                        QueryByClass();
+                        break;
+                }
+            }
+            
+        }
+
+        internal void QueryByPerson()
+        {
+            Console.Clear();
+            GetUserInfo(out string number, out string pwd);
+            MAttendenceCrawler crawler = new MAttendenceCrawler();
+            try
+            {
+                dynamic data = crawler.GetMAttendenceData(number, pwd);
+                string dataStr = MAttendenceCrawler.DataToString(data);
+                Console.WriteLine(dataStr);
+            }
+            catch (LoginException loginEx)
+            {
+                WriteLine("登录失败 : " + loginEx.Message);
+            }
+            catch (WebException ex)
+            {
+                WriteLine("获取信息失败 : " + ex.Message);
+            }
+
+            Console.ReadKey();
+        }
+
+        internal void QueryByClass()
+        {
+            Console.Clear();
+            string classNum = string.Empty;
+            int stuNum = 0;
+            bool inputValidate = false;
+            while(!inputValidate)
+            {
+                Console.Write("输入班级行政号 : ");
+                classNum = Console.ReadLine();
+                if (classNum.Length > 7 || classNum.Length < 6)
+                {
+                    WriteLine("班级行政号只可能是6位或者7位!");
+                    inputValidate = false;
+                    continue;
+                }
+                
+                Console.Write("输入班级人数 : ");
+                if (!int.TryParse(Console.ReadLine(), out stuNum))
+                {
+                    WriteLine("班级人数输入有误!");
+                    inputValidate = false;
+                    continue;
+                }
+                inputValidate = true;
+            }
+            Console.WriteLine();
+
+            MAttendenceCrawler crawler = new MAttendenceCrawler();
+            try
+            {
+                IEnumerable<ExpandoObject> datas = crawler.GetMAttendenceData(classNum, stuNum);
+                datas = datas.OrderBy(
+                    d => d.FirstOrDefault(s => s.Key == "学号").Value as string, StringComparer.OrdinalIgnoreCase).ToList();
+
+                foreach (dynamic d in datas)
+                {
+                    Console.WriteLine(MAttendenceCrawler.DataToString(d));
+                }
+            }
+            catch (LoginException e)
+            {
+                WriteLine("获取信息失败,请检查网络连接和班级行政代码是否正确.内部错误信息 : " + e.Message);
+            }
+            catch (Exception e)
+            {
+                WriteLine("获取信息失败,请检查网络连接和班级行政代码是否正确.内部错误信息 : "+e.Message);
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+
+        public static void GetUserInfo(out string number, out string pwd)
+        {
+            number = pwd = string.Empty;
+            bool inputValid = false;
+            while (!inputValid)
+            {
+                Console.Write("输入学号 : ");
+                number = Console.ReadLine();
+                if(number.Length > 9 || number.Length < 8)
+                {
+                    WriteLine("学号只能是8位或9位");
+                    inputValid = false;
+                    continue;
+                }
+                Console.Write("\r\n输入密码 : ");
+                pwd = Console.ReadLine();
+                inputValid = true;
+            }
+            Console.WriteLine();
+        }
+
+        public static void WriteLine(string text,ConsoleColor textColor = ConsoleColor.DarkRed)
+        {
+            ConsoleColor origin = Console.ForegroundColor;
+            Console.ForegroundColor = textColor;
+            Console.WriteLine(text);
+            Console.ForegroundColor = origin;
+        }
+    }
+}
---
+++ StudentMorningAttendenceCrawler.Core/MAttendenceCrawler.cs
@@ -0,0 +1,174 @@
+﻿using HtmlAgilityPack;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StudentMorningAttendenceCrawler.Core
+{
+    public class LoginException:Exception
+    {
+        public new string Message { get; set; }
+        public new Exception InnerException { get; }
+        public LoginException(string msg,Exception ex)
+        {
+            Message = msg;
+            InnerException = ex;
+        }
+    }
+
+    public class MAttendenceCrawler
+    {
+        private const string LoginPageUrl = "http://10.28.102.51/student/index.jsp";
+        private const string LoginUrl = "http://10.28.102.51/student/checkUser.jsp";
+        private const string PEInfoUrl = "http://10.28.102.51/student/queryExerInfo.jsp";
+
+        /// <summary>
+        /// 利用线程本地存储技术来保证线程安全
+        /// </summary>
+        private ThreadLocal<RestClient> client;
+        private RestClient _client
+        {
+            get => client.Value;
+            set => client.Value = value;
+        }
+
+        public MAttendenceCrawler()
+        {
+            client = new ThreadLocal<RestClient>(() => new RestClient()
+            {
+                CookieContainer = new System.Net.CookieContainer(),
+                Encoding = Encoding.GetEncoding("gb2312")
+            });
+        }
+
+        private IRestResponse Login(string userName, string passwd)
+        {
+            _client.BaseUrl = new Uri(LoginPageUrl);
+            RestRequest loginReq = new RestRequest(Method.GET);
+            _client.Get(loginReq);
+
+            RestRequest req = new RestRequest(Method.POST);
+            req.AddHeader("Accept-Language", "zh-CN,zh;q=0.9,en;q=0.8");
+            req.AddHeader("Accept-Encoding", "gzip, deflate");
+            req.AddHeader("cache-control", "no-cache");
+            req.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+            //request.AddHeader("Cookie", "EC01F06113A100C26442EE128F42250C");
+            req.AddHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36");
+            req.AddParameter("dispalyName", "");
+            req.AddParameter("displayPasswd", "");
+            req.AddParameter("submit", "%B5%C7++%C2%BC");
+            req.AddParameter("userName", userName);
+            req.AddParameter("passwd", passwd);
+
+            _client.BaseUrl = new Uri(LoginUrl);
+
+            IRestResponse res = _client.Execute(req);
+            if (DecodeHtml(res.RawBytes).Contains("登录失败!&nbsp;请重新登录!"))
+                throw new LoginException("登录失败,请检查用户名或密码是否正确", res.ErrorException);
+            else if (res.ErrorException != null)
+                throw res.ErrorException;
+            return res;
+        }
+
+        /// <summary>
+        /// 调用此方法前必须先调用 Login 方法
+        /// </summary>
+        /// <returns>服务器响应</returns>
+        private IRestResponse GetPEInfoPage()
+        {
+            _client.BaseUrl = new Uri(PEInfoUrl);
+            RestRequest req = new RestRequest(Method.GET);
+            IRestResponse res = _client.Get(req);
+            if (res.ErrorException != null)
+                throw res.ErrorException;
+            return res;
+        }
+
+        public ExpandoObject GetMAttendenceData(string userName, string pwd)
+        {
+            try
+            {
+                IRestResponse res = Login(userName, pwd);
+                if (res == null) throw new ArgumentNullException(nameof(res));
+                dynamic info = new ExpandoObject();
+
+                HtmlDocument doc = new HtmlDocument();
+                doc.LoadHtml(Encoding.GetEncoding("gb2312").GetString(res.RawBytes));
+                info.学号 = GetDataItem(doc.DocumentNode, "学号");
+                info.姓名 = GetDataItem(doc.DocumentNode, "姓名");
+                info.性别 = GetDataItem(doc.DocumentNode, "性别");
+                info.年级 = GetDataItem(doc.DocumentNode, "年级");
+                info.行政班级 = GetDataItem(doc.DocumentNode, "行政班级");
+
+                doc.LoadHtml(DecodeHtml(GetPEInfoPage().RawBytes));
+                info.早操 = GetDataItem(doc.DocumentNode, "早操");
+                info.体育俱乐部考勤 = GetDataItem(doc.DocumentNode, "体育俱乐部考勤");
+                info.引体向上考勤 = GetDataItem(doc.DocumentNode, "引体向上考勤");
+                info.篮球比赛 = GetDataItem(doc.DocumentNode, "篮球比赛");
+                info.田径 = GetDataItem(doc.DocumentNode, "田径");
+                info.运动会单项 = GetDataItem(doc.DocumentNode, "运动会单项");
+                info.竞赛管理 = GetDataItem(doc.DocumentNode, "竞赛管理");
+                info.考勤4 = GetDataItem(doc.DocumentNode, "考勤4");
+                info.考勤5 = GetDataItem(doc.DocumentNode, "考勤5");
+                info.增加次数 = GetDataItem(doc.DocumentNode, "增加次数");
+                return info;
+            }
+            catch(LoginException loginEx)
+            {
+                throw loginEx;
+            }
+            catch(Exception e)
+            {
+                throw new WebException("无法正确解析网页,请确保网络联通", e);
+            }
+        }
+
+        public IEnumerable<ExpandoObject> GetMAttendenceData(string classNumber,int stuCount = 40)
+        {
+            List<ExpandoObject> datas = new List<ExpandoObject>();
+            Parallel.For(0, stuCount, (i, state) =>
+            {
+                string key = classNumber + (i + 1).ToString().PadLeft(2, '0');
+                try
+                {   //这里的异常要及时处理掉，否则会影响其他信息的查询
+                    dynamic d = GetMAttendenceData(key, key);
+                    datas.Add(d);
+                }
+                catch
+                {
+                    return;
+                }
+            });
+            return datas;
+        }
+
+        private string DecodeHtml(byte[] data)
+        {
+            return Encoding.GetEncoding("gb2312").GetString(data);
+        }
+
+        public string GetDataItem(HtmlNode docNode, string key)
+        {
+            HtmlNode node = docNode.SelectNodes("//td").FirstOrDefault(n => n.InnerText.Replace("&nbsp;",string.Empty) == key);
+            
+            return node?.ParentNode.SelectSingleNode("td[last()]").InnerText.Replace("&nbsp;", string.Empty);
+        }
+
+        public static string DataToString(ExpandoObject data,bool formatted = true)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach(var i in data)
+            {
+                sb.AppendFormat("{0} : {1}\r\n", i.Key, i.Value);
+            }
+            return sb.ToString();
+        }
+
+    }
+}
